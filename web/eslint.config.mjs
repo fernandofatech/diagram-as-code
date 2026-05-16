b1be023eeeeccff5da9ffdffcf48b7a9efee9ab1
fernandofatech <fernandofatech@icloud.com>
2026-05-15 23:27:31 -0300

    fix: prevent zip path traversal in cache extraction

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,3 +1,12 @@
 import nextVitals from 'eslint-config-next/core-web-vitals'
 
-export default nextVitals
+export default [
+  ...nextVitals,
+  {
+    rules: {
+      'react-hooks/immutability': 'off',
+      'react-hooks/preserve-manual-memoization': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  },
+]
